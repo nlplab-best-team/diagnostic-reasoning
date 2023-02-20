41b8e536be6253c9652d37b7a7b9a25c15f56e5a
Brian-Ckwu <sonny40920710@gmail.com>
2023-02-20 10:33:28 +0800

    feat(ckwu): implement Experiment.calc_acc() function

--- experiment.py
+++ experiment.py
@@ -1,3 +1,5 @@
+import os
+import re
 import sys
 import ast
 import time
@@ -7,6 +9,7 @@ import logging
 import pandas as pd
 from pathlib import Path
 from argparse import Namespace
+from colorama import Fore, Style
 
 from bot import PatientBot, DoctorBot
 from utils import load_all_diagnoses
@@ -78,10 +81,58 @@ class Experiment(object):
         assert role in ["patient", "doctor"]
         (self._log_path / f"{role}-{idx}.json").write_text(json.dumps({"doctor_utters": dialogue._doctor_utters, "patient_utters": dialogue._patient_utters}, indent=4))
     
+    def pathology_to_eng(self, pathology: str):
+        return PatientProfile.release_conditions[pathology]["cond-name-eng"]
+
+    def calc_acc(self, dx_regex: str = r"the most likely diagnosis is (.*)\.", count: int = int(1e8), verbose: bool = False) -> float:
+        # Load ground truths
+        labels = [self.pathology_to_eng(pat.PATHOLOGY) for pat in self._samples.itertuples()][:count]
+        count = min(count, len(labels))
+        # Load logged results
+        filenames = list()
+        exist_files = set(os.listdir(self._log_path))
+        for i in range(1, count + 1):
+            filename = f"doctor-{i}.json"
+            if filename in exist_files:
+                filenames.append(filename)
+            else:
+                raise ValueError(f"File {filename} not found.")
+        assert len(labels) == len(filenames)
+        # Extract predictions
+        preds = list()
+        for filename, label in zip(filenames, labels):
+            d = json.loads((self._log_path / filename).read_bytes())
+            dx = ''
+            # logging.info(filename)
+            for doctor_utter in d["doctor_utters"]:
+                dxs = re.findall(pattern=dx_regex, string=doctor_utter)
+                if len(dxs) == 1:
+                    if not dx:
+                        dx = dxs[0]
+                    else:
+                        if (dx != dxs[0]):
+                            # dx = dxs[0]
+                            logging.warning(f"!!! Diagnosis {dx} not equal to {dxs[0]} in file {filename} !!!")
+                elif len(dxs) > 1:
+                    raise ValueError("There shouldn't be more than 1 match of 'dx_regex'. Please check.")
+            if not dx:
+                raise ValueError("There should be at least 1 diagnosis in each dialouge.")
+            preds.append(dx)
+        assert len(preds) == len(labels)
+        # Comparison
+        ncorrects = 0
+        for pred, label in zip(preds, labels):
+            if verbose:
+                    logging.info(f"Ground Truth: {Fore.RED + label + Style.RESET_ALL} / Prediction: {Fore.BLUE + pred + Style.RESET_ALL}{f' {Fore.GREEN}✔{Style.RESET_ALL}' if (pred == label) else ''}")
+            if pred == label:
+                ncorrects += 1
+        return ncorrects / len(preds)
+
     def run(self, api_interval: int) -> None:
         """
             Run the experiment. The interval between API calls is set to [api_interval] seconds.
         """
+        # TODO: save experiment config (yml)
         # Cost estimation
         cost = self.estimate_cost()
         flag = input(f"Estimated cost: {cost} USD (~{cost * 30} TWD). Continue? (Y)")
@@ -90,7 +141,9 @@ class Experiment(object):
             return
         
         for i, pat in enumerate(self._samples.itertuples()):
-            logging.info(f"===== Running with sample {i + 1} -> PATHOLOGY: {PatientProfile.release_conditions[pat.PATHOLOGY]['cond-name-eng']} =====")
+            if (i < 63): # NOTE: conduct the subset
+                continue
+            logging.info(f"===== Running with sample {i + 1} -> PATHOLOGY: {self.pathology_to_eng(pat.PATHOLOGY)} =====")
             # Initialize the patient
             patient = PatientBot(
                 instruction=self._pat_instruction,
@@ -144,9 +197,11 @@ if __name__ == "__main__":
         handlers=[logging.StreamHandler(sys.stdout)]
     )
     
-    with open("./config.yml", mode="rt") as f:
+    with open(sys.argv[1], mode="rt") as f:
         config = yaml.safe_load(f)
         logging.info(f"Configuration loaded: {json.dumps(config, indent=4)}")
     
     exp = Experiment(**config, debug=False)
-    exp.run(api_interval=10)
+    # exp.run(api_interval=15)
+    acc = exp.calc_acc(count=100, verbose=True)
+    print(f"Accuracy: {acc * 100:.2f}%")
